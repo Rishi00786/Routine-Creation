f65674eb717dec8c79e96edec6faa9c86ede1756
Rishi00786 <rishidhingra52@gmail.com>
2024-12-21 01:53:17 +0530

    updates

--- src/components/Custom/CustomerRoutines.jsx
+++ src/components/Custom/CustomerRoutines.jsx
@@ -133,7 +133,15 @@ const CustomerRoutines = () => {
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {allRoutines.map((routine) => (
-              <Tilt key={routine.id} className="w-full sm:w-[26rem] max-w-[28rem] border border-indigo-300 rounded-2xl hover:shadow-xl hover:shadow-indigo-200 flex flex-col items-center bg-gradient-to-r from-purple-50 to-indigo-50 p-2">
+                <Tilt
+                key={routine.id}
+                className="w-full sm:w-[26rem] max-w-[28rem] border border-indigo-300 rounded-2xl hover:shadow-xl hover:shadow-indigo-200 flex flex-col items-center bg-gradient-to-r from-purple-50 to-indigo-50 p-2"
+                tiltMaxAngleX={5}
+                tiltMaxAngleY={5}
+                perspective={1000}
+                scale={1.05}
+                transitionSpeed={450}
+              >
                 <img
                   src={routine.imagePreview}
                   alt={routine.name}
@@ -141,21 +149,23 @@ const CustomerRoutines = () => {
                 />
                 <div className="mt-2 flex flex-col items-start justify-center w-full p-1">
                   <h4 className="font-bold text-2xl text-indigo-700 main4">{routine.name}</h4>
-                  <p className="mt-3 text-zinc-600 text-lg leading-relaxed main1 text-start">
-                    {routine.description}
-                  </p>
+                  <p className="mt-3 text-zinc-600 text-lg leading-relaxed main1 text-start">{routine.description}</p>
+                  <div className="mt-4 flex flex-col space-y-3 items-start justify-center">
+                    <p className="text-base text-slate-700 main1 font-semibold">
+                      <span className="font-semibold text-indigo-600 main4">Duration:</span> {routine.duration} Weeks
+                    </p>
+                    <p className="text-base text-slate-700 main1 font-semibold">
+                      <span className="font-semibold text-indigo-600 main4">Milestones:</span> {routine.milestones}
+                    </p>
+                  </div>
                   <div className="mt-6 w-full text-center">
-                    <button
-                      type="button"
-                      className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2"
-                      onClick={() => handleGetRoutineStarted(routine.id)}
-                    >
-                      Get Started
-                    </button>
+                    <div onClick={()=>{handleGetRoutineStarted(routine.id)}}><button type="button" className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 shadow-lg shadow-pink-500/50 dark:shadow-lg dark:shadow-pink-800/80 font-medium rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2">
+                    Get Started
+                  </button></div>
                   </div>
                 </div>
               </Tilt>
-            ))}
+              ))}
           </div>
         )}
       </div>
